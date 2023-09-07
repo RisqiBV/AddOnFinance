e6e6f6f8ad12128a066d181b939a29939f172379
Risqi Bagus <risqibaguspahlevi@gmail.com>
2023-09-07 09:21:29 +0700

    Add project files.

---
+++ AddOnFinanceWeb/Views/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace AddOnFinanceWeb.Views.Data
+{
+    public class ApplicationDBContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
+        
+        }                      
+    }
+}
